6d9be2bac847c0dc4e9b606d92e87ff2674ba215
robertmirro <rmirro@gmail.com>
2014-09-15 20:14:00 -0400

    img.src using ng-src directive

--- shaping-up-with-angular/levels/2.1/app.js
+++ shaping-up-with-angular/levels/2.1/app.js
@@ -11,14 +11,26 @@
             price : 2 ,
             description : 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.' , 
             canPurchase : true , 
-            soldOut : false
+            soldOut : false , 
+            images : [
+                { 
+                    full : '../../angular_full.png' , 
+                    thumb : '../../angular_thumb.png' 
+                }
+            ]
         } , 
         {
             name : 'Pentagonal Gem' ,
             price : 5.95 ,
             description : 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.' , 
             canPurchase : false , 
-            soldOut : false
+            soldOut : false ,
+            images : [
+                { 
+                    full : '../../angular_full.png' , 
+                    thumb : '../../angular_thumb.png' 
+                }
+            ]
         } , 
     ];
 })();
