6dd558c6cdc3957566251e06c0845cab46454a21
robertmirro <rmirro@gmail.com>
2014-09-27 18:22:25 -0400

    use array syntax for dependency injection of controller

--- planning-for-aliens/lesson-three-1.js
+++ planning-for-aliens/lesson-three-1.js
@@ -3,12 +3,25 @@
 
     var app = angular.module( 'HelloWorld' , [] );
 
-    // use DI to inject $scope and $rootScope into controller
-    app.controller( 'World' , function( $scope , $rootScope ) {
-        console.log( '$rootScope:' , $rootScope );
-        $scope.text = 'World';
+    // use angular DI to inject $scope and $rootScope into controller, add $compile too
+    // NOTE: these objects are not generally available unless they are injected 
+    // NOTE: the order of the injected objects does not matter
+    // app.controller( 'World' , function( $scope , $rootScope , $compile ) {
+    //     console.log( '$rootScope:' , $rootScope );
+    //     $scope.text = 'World';
+    //     console.log( $scope );
+    //     console.log( $compile );
+    // });
+
+    // could also use array syntax to specify DI for the controller
+    // NOTE: this is similar to AMD and now ORDER MATTERS
+    app.controller( 'World' , [ '$scope' , '$rootScope' , '$compile' , function( $scope , $rootScope , $compile ) {
+        $scope.text = 'Worldddd';
         console.log( $scope );
-    });
+
+        console.log( '$rootScope:' , $rootScope );
+        console.log( $compile );
+    }]);
 
     app.directive( 'hello' , function() {
         return {
