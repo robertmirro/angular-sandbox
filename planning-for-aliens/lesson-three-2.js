001b8dc9c7023e14feb6b72f5b44d6a9854e35ec
robertmirro <rmirro@gmail.com>
2014-09-27 19:13:01 -0400

    clean up DIY injector code

--- planning-for-aliens/lesson-three-2.js
+++ planning-for-aliens/lesson-three-2.js
@@ -1,37 +1,7 @@
 (function () {
     'use strict';
 
-    // // specify our namespace as "HelloWorld"
-    // // NOTE: angulars internam namespace is "ng"
-    // var app = angular.module( 'HelloWorld' , [] );
-
-    // // use angular DI to inject $scope and $rootScope into controller, add $compile too
-    // // NOTE: these objects are not generally available unless they are injected 
-    // // NOTE: the order of the injected objects does not matter
-    // // app.controller( 'World' , function( $scope , $rootScope , $compile ) {
-    // //     console.log( '$rootScope:' , $rootScope );
-    // //     $scope.text = 'World';
-    // //     console.log( $scope );
-    // //     console.log( $compile );
-    // // });
-
-    // // could also use array syntax to specify DI for the controller
-    // // NOTE: this is similar to AMD and now ORDER MATTERS
-    // app.controller( 'World' , [ '$scope' , '$rootScope' , '$compile' , function( $scope , $rootScope , $compile ) {
-    //     $scope.text = 'Worldddd';
-    //     console.log( $scope );
-
-    //     console.log( '$rootScope:' , $rootScope );
-    //     console.log( $compile );
-    // }]);
-
-    // app.directive( 'hello' , function() {
-    //     return {
-    //         restrict : 'A' ,
-    //         template : '<input ng-model="text" /><p>Hello {{ text }}!</p>' ,
-    //         controller : 'World'
-    //     }
-    // });
+    // implement a DIY injector
 
     var namespace = {
         HelloWorld : {
@@ -44,31 +14,35 @@
     var injector = function injector( name ) {
         var _namespace = namespace[ name ];
         // console.log( '_namespace:' , _namespace );
-        _namespace.hello();
-
+        
         return {
             invoke : function invoke( input ) {
                 // assume cb is last element of "input" array
                 var cb = input[ input.length - 1 ];
+
+                // get all args before cb
                 var args = input.splice( 0 , input.length - 1 );
                 // console.log( 'args:' , args );
 
+                // populate "funcs" with reference to each namespace function which was passed as an arg to .invoke()
                 var funcs = args.map( function map( arg ) {
                     return _namespace[ arg ];
                 });
-                console.log( 'funcs:' , funcs );
+                // console.log( 'funcs:' , funcs );
 
-                cb( _namespace.hello );
+                // invoke cb and pass array of functions as params that cb needs to call
+                cb.apply( null , funcs );
             }
         };
     };
 
-    // specify that our injector should use the "HelloWorld" namespace
+    // specify that our DIY injector should use the "HelloWorld" namespace
     var myInjector = injector( 'HelloWorld' );
 
     // we want access to namespace.HelloWorld function(s) 
-    myInjector.invoke( [ 'hello' , 'hello again...' , function( hello ) {
+    myInjector.invoke( [ 'hello' , 'someOtherMethod' , function( hello ) {
         console.log( 'myInjector.invoke()' );
+        console.log( 'args passed:' , arguments );
         hello();
     }]);
 
