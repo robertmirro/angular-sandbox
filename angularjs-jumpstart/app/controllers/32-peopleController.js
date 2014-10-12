c441834a06136a82079c7aebf42bfb0706be2be2
robertmirro <rmirro@gmail.com>
2014-10-11 21:05:17 -0400

    fix to work with angular 1.3 and higher

--- angularjs-jumpstart/app/controllers/32-peopleController.js
+++ angularjs-jumpstart/app/controllers/32-peopleController.js
@@ -1,19 +1,26 @@
-// $scope is dynamically injected into controller
-function PeopleController( $scope ) {
-    $scope.people = [
-        {name : 'Robert' , city : 'Alexandria' , rate : 9.99 , joined : '2014-01-10' } ,
-        {name : 'Rob' , city : 'DC' , rate : 4.84 , joined : '2014-02-04' } ,
-        {name : 'Bob' , city : 'Philly' , rate : 6.9 , joined : '2014-04-08' } ,
-        {name : 'Bobby' , city : 'Scranton' , rate : 48.12345 , joined : '2014-08-04' }
-    ];
+(function () {
+    'use strict';
 
-    // set defaults
-    $scope.sortBy = 'name';
-    $scope.reverse = false;                
+    var app = angular.module( 'peopleApp' , [] );
 
-    // create a sortBy var to store sort value (people.<property>), create reverse var and toggle it
-    $scope.sortColumn = function( propertyName ) {
-        $scope.sortBy = propertyName;
-        $scope.reverse = !$scope.reverse;
-    };
-}
+    // $scope is dynamically injected into controller
+    app.controller( 'PeopleController' , function ( $scope ) {
+        $scope.people = [
+            {name : 'Robert' , city : 'Alexandria' , rate : 9.99 , joined : '2014-01-10' } ,
+            {name : 'Rob' , city : 'DC' , rate : 4.84 , joined : '2014-02-04' } ,
+            {name : 'Bob' , city : 'Philly' , rate : 6.9 , joined : '2014-04-08' } ,
+            {name : 'Bobby' , city : 'Scranton' , rate : 48.12345 , joined : '2014-08-04' }
+        ];
+
+        // set defaults
+        $scope.sortBy = 'name';
+        $scope.reverse = false;                
+
+        // create a sortBy var to store sort value (people.<property>), create reverse var and toggle it
+        $scope.sortColumn = function( propertyName ) {
+            $scope.sortBy = propertyName;
+            $scope.reverse = !$scope.reverse;
+        };
+    });
+    
+})();
