c13933d47f036f71010a3040cc0bb647aead38d2
robertmirro <rmirro@gmail.com>
2014-10-12 21:02:00 -0400

    factory

---
+++ angularjs-jumpstart/app/controllers/56-peopleController.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    // define controller function seperately
+    function PeopleController( $scope , PeopleFactory ) {
+
+        // set defaults
+        $scope.sortBy = 'name';
+        $scope.reverse = false;                
+        
+        // get people from factory
+        $scope.people = PeopleFactory.getPeople();
+        
+        // create a sortBy var to store sort value (people.<property>), create reverse var and toggle it
+        $scope.sortColumn = function( propertyName ) {
+            $scope.sortBy = propertyName;
+            $scope.reverse = !$scope.reverse;
+        };
+    };
+    
+    // reference app using angular.module( appname ), bind controller
+    // use array param to allow dependency injection with minification
+    angular.module( 'peopleApp' )
+        .controller( 'PeopleController' , [ '$scope' , 'PeopleFactory' , PeopleController ] );
+    
+})();
---
+++ angularjs-jumpstart/app/modules/56-peopleApp.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    // no need to store module reference in var
+    // add ngRoute dependency in order to access $routeProvider
+    angular.module( 'peopleApp' , [ 'ngRoute' ] )
+    
+        // inject $routeProvider
+        // routing marries a controller to view
+        .config( function ( $routeProvider ) {
+            $routeProvider
+                .when( '/' , {
+                    controller : 'PeopleController' , 
+                    templateUrl : 'app/views/56-people.html'
+                })
+                .when( '/orders/:personId' , {
+                    controller : 'OrdersController' , 
+                    templateUrl : 'app/views/56-orders.html'
+                })
+                .otherwise( {
+                    redirectTo : '/'
+                })
+        });
+    
+})();
