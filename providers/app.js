277697234d3b047e4f589dcc6f09a412438608dc
robertmirro <rmirro@gmail.com>
2014-10-15 21:49:37 -0400

    name anonymous functions

--- providers/app.js
+++ providers/app.js
@@ -7,18 +7,18 @@
     
     angular.module( 'providers' , [] )
         .provider( 'aProvider' , { $get : 
-            function() {
+            function aProvider() {
                 return 'Provider data...';
             }
         })
-        .factory( 'aFactory' , function() {
+        .factory( 'aFactory' , function aFactory() {
             return 'Factory data...';
         }) 
         .service( 'aService', serviceConstructor )
-        .value( 'aValue' , 'Value value...')
+        .value( 'aValue' , 'Value data...')
         .controller( 'aController' , 
         [ '$scope' , 'aProvider' , 'aFactory' , 'aService' , 'aValue' ,  
-            function( $scope , aProvider , aFactory , aService , aValue ) {
+            function aController( $scope , aProvider , aFactory , aService , aValue ) {
                 $scope.providerValue = aProvider;
                 $scope.factoryValue = aFactory;
                 $scope.serviceValue = aService.serviceValue;
