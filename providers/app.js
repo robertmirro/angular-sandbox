757f7904cf64c56b23f8a67f6b0be357398fc3c5
robertmirro <rmirro@gmail.com>
2014-10-15 21:35:36 -0400

    provider and controller

--- providers/app.js
+++ providers/app.js
@@ -2,5 +2,14 @@
     'use strict';
 
     angular.module( 'providers' , [] )
+        .provider( 'aProvider' , { $get : function() {
+            return 'I am a Provider';
+        }})
+        .controller( 'aController' , 
+        [ '$scope' , 'aProvider' ,  
+            function( $scope , aProvider ) {
+                $scope.providerValue = aProvider;
+            }
+        ])
     ;
 })();
