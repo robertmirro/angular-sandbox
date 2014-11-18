fff4b7ee194aecd9761b2fca0157b934104d6df4
robertmirro <rmirro@gmail.com>
2014-11-18 17:35:52 -0500

    $rootScope

--- planning-for-aliens/lesson-one-2.js
+++ planning-for-aliens/lesson-one-2.js
@@ -5,12 +5,14 @@
 
     console.log( app );
 
-    app.controller( 'world' , function( $scope ) {
+    app.controller( 'world' , function( $scope , $rootScope ) {
         $scope.oldText = 'Old World';
         $scope.w.anotherValue = 'another value';
         this.text = 'World';
         this.otherText = 'Other World';
         console.log( $scope );
+        console.log( $rootScope );
+        console.log( '$scope.$parent.$root === $rootScope:' , $scope.$parent.$root === $rootScope );
     });
 
 })();
