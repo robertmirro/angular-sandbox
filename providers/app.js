34b7aebaf7f16d1b7db8100044f56d1cb40f21f1
robertmirro <rmirro@gmail.com>
2014-12-15 10:52:20 -0500

    $q promise chaining

--- providers/app.js
+++ providers/app.js
@@ -17,12 +17,42 @@
         .service( 'aService', serviceConstructor )
         .value( 'aValue' , 'Value data...')
         .controller( 'aController' , 
-        [ '$scope' , 'aProvider' , 'aFactory' , 'aService' , 'aValue' ,  
-            function aController( $scope , aProvider , aFactory , aService , aValue ) {
+        [ '$scope' , 'aProvider' , 'aFactory' , 'aService' , 'aValue' , '$q' ,  
+            function aController( $scope , aProvider , aFactory , aService , aValue, $q ) {
                 $scope.providerValue = aProvider;
                 $scope.factoryValue = aFactory;
                 $scope.serviceValue = aService.serviceValue;
                 $scope.valueValue = aValue;
+
+                $scope.clickMe = function() {
+                    console.log('clicked...' + Date());
+
+                    getData().then(function(data) {
+                        console.log('data from clickMe:',data);
+                    });    
+                }    
+
+                function getData() {
+                    console.log('getting data...');
+
+                    return getPromise().then(function(promiseData){
+                        console.log('promiseData from getData:',promiseData);
+                        return promiseData;
+                    });    
+                }
+
+                function getPromise() {
+                    console.log('getting promise...');
+
+                    var deferred = $q.defer();
+                    setTimeout( function() {
+                        var deferredResolved = 'Resolved from getPromise:' + Date();
+                        deferred.resolve( deferredResolved );        
+                        console.log(deferredResolved);
+                    } , 2000 );
+                    console.log('deferred created from getPromise:',deferred);
+                    return deferred.promise;                    
+                }
 
                 setTimeout( function() { 
                     aValue += 'NEW VALUE...';
@@ -30,7 +60,7 @@
                     $scope.$apply();
                     console.log($scope);
                     console.log( 'aValue:' , aValue );
-                } , 3000 );
+                } , 1000 );
 
             }
         ]);
