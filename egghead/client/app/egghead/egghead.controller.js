8833a9a8409e0522e4deb40544bc074219707bdb
robertmirro <rmirro@gmail.com>
2015-07-09 18:00:06 -0400

    filter: reverseMe/It

--- egghead/client/app/egghead/egghead.controller.js
+++ egghead/client/app/egghead/egghead.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
-angular.module('eggheadApp')
-  .controller('EggheadCtrl', function($scope) {
-      $scope.message = 'Hello';
-  });
+angular
+    .module('eggheadApp')
+    .controller('EggheadCtrl', function($scope) {
+        $scope.message = 'Hello';
+        $scope.reverseMe = reverseMe;
+
+        function reverseMe(value) {
+            return value.split('').reverse().join('');
+        }
+    })
+    .filter('reverseIt', function() {
+        return function(text, fn) {
+            return fn(text);
+        };
+    });
