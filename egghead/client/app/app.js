678a668b1597eefe6adfaf588ec998410d5630a7
robertmirro <rmirro@gmail.com>
2015-07-09 17:29:58 -0400

    refactor

--- egghead/client/app/app.js
+++ egghead/client/app/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
-angular.module('eggheadApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ui.router',
-    'ui.bootstrap'
+angular
+    .module('eggheadApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+        'ui.bootstrap'
     ])
 
     .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -13,20 +14,5 @@ angular.module('eggheadApp', [
           .otherwise('/');
 
         $locationProvider.html5Mode(true);
-    })
-
-    .controller('egg_1', function($scope) {
-        var $vm = $scope;
-
-        $vm.dot = {
-            field: '$scope.dot.field'
-        };
-        $vm.field = '$scope.field';
-        console.log('$scope 1:', $scope, '- $scope === this?', $scope === this);
-    })
-
-    .controller('egg_2', function($scope) {
-        // $scope.theScope = 'egg_1 scope';
-        console.log('$scope 2:', $scope, '- $scope === this?', $scope === this);
     });
 
--- egghead/client/app/egghead/egghead.js
+++ egghead/client/app/egghead/egghead.js
@@ -1,12 +1,27 @@
 'use strict';
 
-angular.module('eggheadApp')
-  .config(function($stateProvider) {
-      $stateProvider
-          .state('egghead', {
-              url: '/egghead',
-              templateUrl: 'app/egghead/egghead.html',
-              controller: 'EggheadCtrl'
-          });
-  });
+angular
+    .module('eggheadApp')
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('egghead', {
+                url: '/egghead',
+                templateUrl: 'app/egghead/egghead.html',
+                controller: 'EggheadCtrl'
+            });
+    })
 
+    .controller('egg_1', function($scope) {
+        var $vm = $scope;
+
+        $vm.dot = {
+            field: '$scope.dot.field'
+        };
+        $vm.field = '$scope.field';
+        console.log('$scope 1:', $scope, '- $scope === this?', $scope === this);
+    })
+
+    .controller('egg_2', function($scope) {
+        // $scope.theScope = 'egg_1 scope';
+        console.log('$scope 2:', $scope, '- $scope === this?', $scope === this);
+    });
