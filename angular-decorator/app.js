1cd90c14d6b331e6e99472fbb203468a55c7c7c6
robertmirro <rmirro@gmail.com>
2016-03-29 15:25:52 -0400

    incremental

--- angular-decorator/app.js
+++ angular-decorator/app.js
@@ -5,8 +5,22 @@
         .module('theApp', [])
         .controller('theController', theController)
         .provider('theProvider', theProvider)
+        .config(theProviderConfig2)
         .provider('theProvider', theOtherProvider)
-        .decorator('theProvider', theProviderDecorator);
+        .decorator('theProvider', theProviderDecorator)
+        .config(theProviderConfig1);
+
+    function theProviderConfig1(theProviderProvider) {
+        console.log('theProviderConfig1...', theProviderProvider.getInternalProperty());
+
+        theProviderProvider.setInternalProperty('-theProviderConfig1');
+    }
+
+    function theProviderConfig2(theProviderProvider) {
+        console.log('theProviderConfig2...', theProviderProvider.getInternalProperty());
+
+        theProviderProvider.setInternalProperty('-theProviderConfig2');
+    }
 
     function theController(theProvider) {
         console.log('theController...');
@@ -19,6 +33,9 @@
 
         var theInternalProperty = 'theProviderInternal';
 
+        this.getInternalProperty = function() {
+            return theInternalProperty;
+        };
         this.setInternalProperty = function(value) {
             theInternalProperty += value;
         };
@@ -35,6 +52,9 @@
 
         var theInternalProperty = 'theOtherProviderInternal';
 
+        this.getInternalProperty = function() {
+            return theInternalProperty;
+        };
         this.setInternalProperty = function(value) {
             theInternalProperty += value;
         };
