58f3460b61265db09d9fa0831cfc5c69640cad5c
robertmirro <rmirro@gmail.com>
2016-03-29 15:54:09 -0400

    incremental

--- angular-decorator/app.js
+++ angular-decorator/app.js
@@ -12,34 +12,32 @@
         .config(theProviderConfig1);
 
     function theProviderConfig1(theProviderProvider) {
-        console.log('theProviderConfig1...\n  ', theProviderProvider.getInternalProperty());
+        console.log('theProviderConfig1...');
 
+        console.log('  BEFORE:', theProviderProvider.getInternalProperty());
         theProviderProvider.setInternalProperty('-theProviderConfig1');
+        console.log('  AFTER: ', theProviderProvider.getInternalProperty());
     }
 
     function theProviderConfig2(theProviderProvider) {
-        console.log('theProviderConfig2...\n  ', theProviderProvider.getInternalProperty());
+        console.log('theProviderConfig2...');
 
+        console.log('  BEFORE:', theProviderProvider.getInternalProperty());
         theProviderProvider.setInternalProperty('-theProviderConfig2');
+        console.log('  AFTER: ', theProviderProvider.getInternalProperty());
     }
 
     function theProviderConfigDecorator($provide) {
-        console.log('theProviderConfigDecorator...');
+        console.log('theProviderConfigDecorator... LOADED');
 
         $provide.decorator('theProvider', function($delegate) {
-            console.log('theProviderConfigDecorator - $provide.decorator...\n  ', $delegate);
+            console.log('theProviderConfigDecorator - DECORATOR...\n  ', $delegate);
 
             $delegate.theInternalProperty += '-theProviderConfigDecorator';
             $delegate.theProperty += '-theProviderConfigDecorator';
             $delegate.theDecoratedProperty = ($delegate.theDecoratedProperty || '') + ($delegate.theDecoratedProperty ? ' - ' : '') + 'theProviderConfigDecorator';
             return $delegate;
         });
-    }
-
-    function theController(theProvider) {
-        console.log('theController...');
-        this.dateTime = new Date();
-        this.theProvider = theProvider;
     }
 
     function theProvider() {
@@ -86,4 +84,11 @@
         $delegate.theDecoratedProperty = ($delegate.theDecoratedProperty || '') + ($delegate.theDecoratedProperty ? ' - ' : '') + 'theProviderDecorator';
         return $delegate;
     }
+
+    function theController(theProvider) {
+        console.log('theController...');
+        this.dateTime = new Date();
+        this.theProvider = theProvider;
+    }
+
 })();
