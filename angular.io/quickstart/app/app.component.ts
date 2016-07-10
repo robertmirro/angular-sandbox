5ee1f85762ecccb88f16adbd723f3e0352e2a8e0
robertmirro <rmirro@gmail.com>
2016-07-10 18:30:50 -0400

    ng2 quickstart

---
+++ angular.io/quickstart/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import * as ngCore from '@angular/core';
+console.log('ngCore:', ngCore);
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App...</h1>'
+})
+export class AppComponent { }
