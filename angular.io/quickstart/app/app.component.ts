c9a01d5be1cabef7d596df14480b4e29e816e293
robertmirro <rmirro@gmail.com>
2016-07-11 12:32:54 -0400

    hero details

--- angular.io/quickstart/app/app.component.ts
+++ angular.io/quickstart/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 import * as ngCore from '@angular/core';
 console.log('ngCore:', ngCore);
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App...</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details:</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Mirros';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    }
+}
